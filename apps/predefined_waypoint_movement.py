776374624e6793836f8bc105a25e2b679b9a5645
mahp <mahp@universal-robots.com>
2021-12-10 16:03:49 +0100

    Added kinematic models for all the different robot models
    * Added subgraphs for all the different robot models
    
    * Updated applications and documentation accordingly to these changes

--- apps/predefined_waypoint_movement.py
+++ apps/predefined_waypoint_movement.py
@@ -3,13 +3,15 @@ import argparse
 import json
 import time
 
+from numpy.lib.twodim_base import tri
+
 from isaac import Application, Cask
 from packages.pyalice.Composite import create_composite_message
+
 '''
-Moves a robot arm based on joint waypoints to pickup and dropoff a box between two pre-defined
-locations repeatedly. This app assumes a vacuum pump is connected through the digital
-io interfaces and that the robot is an UR10 model. This is tested with a robot, without a gripper connected.
-This application might be removed in a future release.
+An app that moves a UR robot, between 6 predefined waypoints, this app is based on the shuffle_box_hardware.
+However, this app can be used for all robot models, with the correct configuration of the kinematic model.
+This app doesn't assume any gripper is attached to the robot.
 '''
 
 
@@ -23,7 +25,7 @@ def create_composite_waypoint(name, quantities, values):
 def create_composite_atlas(cask_root, joints):
     '''Creates composite atlas cask with waypoints for ur10.'''
     if len(joints) != 6:
-        raise ValueError("UR10 should have 6 joints, got {}".format(len(joints)))
+        raise ValueError("UR robot should have 6 joints, got {}".format(len(joints)))
 
     cask = Cask(cask_root, writable=True)
     # at joint waypoints
@@ -52,25 +54,43 @@ def create_composite_atlas(cask_root, joints):
     cask.write_message(
         create_composite_waypoint("dolly_dropoff", quantities, DOLLY_DROPOFF_WAYPOINT))
 
-    quantities = [[x, "none", 1] for x in ["pump", "valve", "gripper"]]
-    SUCTION_ON_WAYPOINT = np.array([1.0, 0.0, 1.0], dtype=np.dtype("float64"))
-    SUCTION_OFF_WAYPOINT = np.array([0.0, 1.0, 0.0], dtype=np.dtype("float64"))
-    VALVE_OFF_WAYPOINT = np.array([0.0, 0.0, 0.0], dtype=np.dtype("float64"))
-    cask.write_message(create_composite_waypoint("suction_on", quantities, SUCTION_ON_WAYPOINT))
-    cask.write_message(create_composite_waypoint("suction_off", quantities, SUCTION_OFF_WAYPOINT))
-    cask.write_message(create_composite_waypoint("valve_off", quantities, VALVE_OFF_WAYPOINT))
-
 if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser(description="Sortbot Demo")
     parser.add_argument("--cask", help="Path to output atlas", default="/tmp/shuffle_box_waypoints")
+    parser.add_argument("--robot_model", help="Robot model.", choices=["ur3", "ur5", "ur10", "ur3e", "ur5e", "ur10e", "ur16e"], default="ur5e")
     parser.add_argument("--robot_ip", help="robot ip", default="192.168.56.101")
     parser.add_argument("--headless_mode", help="start driver with headless mode enabled or not",
                         default=False, type=lambda x: (str(x).lower() == 'true'))
     args = parser.parse_args()
 
-    # get kinematic file and joints
-    kinematic_file = "apps/assets/kinematic_trees/ur10.kinematic.json"
+    # Load driver subgraph and kinematic model
+    robot_model = args.robot_model
+    if robot_model == "ur3":
+        ur_subgraph = "packages/universal_robots/ur_robot_driver/apps/ur3.subgraph.json"
+        kinematic_file = "packages/universal_robots/ur_robot_driver/config/ur3.kinematic.json"
+    elif robot_model == "ur5":
+        ur_subgraph = "packages/universal_robots/ur_robot_driver/apps/ur5.subgraph.json"
+        kinematic_file = "packages/universal_robots/ur_robot_driver/config/ur5.kinematic.json"
+    elif robot_model == "ur10":
+        ur_subgraph = "packages/universal_robots/ur_robot_driver/apps/ur10.subgraph.json"
+        kinematic_file = "packages/universal_robots/ur_robot_driver/config/ur10.kinematic.json"
+    elif robot_model == "ur3e":
+        ur_subgraph = "packages/universal_robots/ur_robot_driver/apps/ur3e.subgraph.json"
+        kinematic_file = "packages/universal_robots/ur_robot_driver/config/ur3e.kinematic.json"
+    elif robot_model == "ur5e":
+        ur_subgraph = "packages/universal_robots/ur_robot_driver/apps/ur5e.subgraph.json"
+        kinematic_file = "packages/universal_robots/ur_robot_driver/config/ur5e.kinematic.json"
+    elif robot_model == "ur10e":
+        ur_subgraph = "packages/universal_robots/ur_robot_driver/apps/ur10e.subgraph.json"
+        kinematic_file = "packages/universal_robots/ur_robot_driver/config/ur10e.kinematic.json"
+    elif robot_model == "ur16e":
+        ur_subgraph = "packages/universal_robots/ur_robot_driver/apps/ur16e.subgraph.json"
+        kinematic_file = "packages/universal_robots/ur_robot_driver/config/ur16e.kinematic.json"
+    else:
+        raise Exception("Unknown robot model")
+
+    # get joints
     joints = []
     with open(kinematic_file, 'r') as fd:
         kt = json.load(fd)
@@ -78,16 +98,13 @@ if __name__ == '__main__':
             if 'motor' in link and link['motor']['type'] != 'constant':
                 joints.append(link['name'])
 
-    ur_subgraph = "packages/universal_robots/ur_robot_driver/apps/ur10.subgraph.json"
-
     # create composite atlas
     create_composite_atlas(args.cask, joints)
 
     # Create and start the app
-    app = Application(name="Shuffle Box Hardware", modules=["sight"])
-    # load bebavior subgraph. this contains the sequency behavior to move the arm between
-    # waypoints and control gripper digital output
-    app.load("packages/universal_robots/apps/shuffle_box_behavior_hardware.subgraph.json", prefix="behavior")
+    app = Application(name="Predefined waypoint movement", modules=["sight"])
+    # load bebavior subgraph. this contains the sequency behavior to move the arm between the waypoints
+    app.load("packages/universal_robots/apps/predefined_waypoint_movement.subgraph.json", prefix="behavior")
     behavior_interface = app.nodes["behavior.interface"]["subgraph"]
     app.nodes["behavior.atlas"]["CompositeAtlas"].config.cask = args.cask
 
@@ -97,20 +114,16 @@ if __name__ == '__main__':
     ur_interface = app.nodes["ur.subgraph"]["interface"]
     ur_controller = app.nodes["ur.controller"]["ScaledMultiJointController"]
     ur_driver = app.nodes["ur.universal_robots"]["UniversalRobots"]
-    app.nodes["ur.kinematic_tree"]["KinematicTree"].config.kinematic_file = kinematic_file
 
     # Configs
     ur_controller.config.control_mode = "joint position"
     ur_driver.config.control_mode = "joint position"
     ur_driver.config.robot_ip = args.robot_ip
     ur_driver.config.headless_mode = args.headless_mode
-    ur_driver.config.tool_digital_out_names = ["valve", "pump"]
-    ur_driver.config.tool_digital_in_names = ["unknown", "gripper"]
+
 
     # Edges
     app.connect(ur_interface, "arm_state", behavior_interface, "joint_state")
-    app.connect(ur_interface, "io_state", behavior_interface, "io_state")
-    app.connect(behavior_interface, "io_command", ur_interface, "io_command")
     app.connect(behavior_interface, "joint_target", ur_interface, "joint_target")
 
     # Sequence nodes
@@ -119,18 +132,27 @@ if __name__ == '__main__':
     nodes_stopped = True
 
     # Enable sight
-    widget = app.add("sight").add(app.registry.isaac.sight.SightWidget, "IO")
+    widget = app.add("sight").add(app.registry.isaac.sight.SightWidget, "predefined_waypoint_movement")
     widget.config.type = "plot"
     widget.config.channels = [
-      {
-        "name": "ur.universal_robots/UniversalRobots/gripper"
-      },
-      {
-        "name": "ur.universal_robots/UniversalRobots/pump"
-      },
-      {
-        "name": "ur.universal_robots/UniversalRobots/valve"
-      }
+        {
+            "name": "ur.universal_robots/UniversalRobots/shoulder_pan_joint"
+        },
+        {
+            "name": "ur.universal_robots/UniversalRobots/shoulder_lift_joint"
+        },
+        {
+            "name": "ur.universal_robots/UniversalRobots/elbow_joint"
+        },
+        {
+            "name": "ur.universal_robots/UniversalRobots/wrist_1_joint"
+        },
+        {
+            "name": "ur.universal_robots/UniversalRobots/wrist_2_joint"
+        },
+        {
+            "name": "ur.universal_robots/UniversalRobots/wrist_4_joint"
+        }
     ]
 
     # run app
